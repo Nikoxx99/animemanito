891d5a16f92891acf6241ed0f6d32c23009a8612
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-12-23 19:28:48 -0500

    wu2

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('UTC');
 $env = getenv('ENV');
-echo 'uwu' . $env;
+echo 'uwu' . $_ENV;
 die();
 $host = ($env === 'DEV' ? 'animemanito.test' : 'animemanito.tv');
 function getSiteDomain()
