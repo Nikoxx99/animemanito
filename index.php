d2ba8f009c9ab7653821ad23c9b3ef8862520b9a
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-12-23 19:18:08 -0500

    test 2

--- index.php
+++ index.php
@@ -3,5 +3,6 @@ require_once('app/config.php');
 require_once('app/session.php');
 require_once('app/functions.php');
 require_once('app/dbconnection.php');
+echo $config['urlpath'];
 // header("Location: ".$config['urlpath']."/home1");
 ?>
